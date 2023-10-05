52edf280f10afe53222c1e0d4051487a6aa50307
HajiMohamedRufai <mohamedrufai59@gmail.com>
2023-10-05 18:35:57 +0300

    -m

--- dags/example-dag.py
+++ dags/example-dag.py
@@ -1,24 +1,37 @@
-from airflow import DAG
-from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-# Define default_args to specify the DAG's behavior
-default_args = {
-    'owner': 'hajirufai',
-    'start_date': datetime(2023, 1, 1),
-    'retries': 1,
-}
+# Define a function that will be executed by PythonOperator
+def extract_data():
+    # Your data extraction logic here
+    pass
 
-# Create the DAG object
-dag = DAG(
-    'etl_example',
-    default_args=default_args,
-    description='A simple ETL DAG',
-    schedule_interval=None,  # You can specify a schedule here (e.g., '@daily')
+def transform_data():
+    # Your data transformation logic here
+    pass
+
+def load_data():
+    # Your data loading logic here
+    pass
+
+# Create task instances for each step
+extract_task = PythonOperator(
+    task_id='extract_data',
+    python_callable=extract_data,
+    dag=dag,
 )
 
-# Define tasks
-start_task = DummyOperator(task_id='start', dag=dag)
+transform_task = PythonOperator(
+    task_id='transform_data',
+    python_callable=transform_data,
+    dag=dag,
+)
 
-   
+load_task = PythonOperator(
+    task_id='load_data',
+    python_callable=load_data,
+    dag=dag,
+)
+
+# Define the order of task execution
+start_task >> extract_task >> transform_task >> load_task
